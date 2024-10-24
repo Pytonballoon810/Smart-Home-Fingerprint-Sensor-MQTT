575d99d83dd62dd5fb5ce42a24b21b3a8de08d64
Philipp Hofmann <philipp@hofmann-ebs.de>
2024-10-24 05:36:32 +0200

    Create arduino_secrets_example.h

---
+++ esp8266-cpp/arduino_secrets_example.h
@@ -0,0 +1,13 @@
+#ifndef SECRET_H  // Check if SECRET_H is not defined
+#define SECRET_H  // Define SECRET_H
+
+#include <string>
+
+const std::string SECRET_SSID = "ssid";
+const std::string SECRET_WIFI_PASSWORD = "pw";
+const std::string SECRET_MQTT_SERVER = "69.69.69.69";
+const std::string SECRET_MQTT_USER = "mqtt-user";
+const std::string SECRET_MQTT_PASSWORD = "mqtt-password";
+const std::string SECRET_SENSOR_PASSWORD = "sensor-password-for-secure-use";
+
+#endif // End of the include guard
